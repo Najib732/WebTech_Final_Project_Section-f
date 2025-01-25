28ade908f4cb1c7b39fd3025fb69da0194b2d78f
Najib Mahfuj <22-48248-2@student.aiub.edu>
2025-01-25 23:02:22 +0600

    FInal Submission

--- Webtech/User/View/UserProfile/Help/contact.php
+++ Webtech/User/View/UserProfile/Help/contact.php
@@ -21,52 +21,7 @@
         <a href="../userprofile.php?id=<?php echo $id; ?>">Profile</a>
     </form>
 
-    <script>
-      
-        document.getElementById('reportForm').addEventListener('submit', function(event) {
-           
-            event.preventDefault();
-
-            // Collect form data
-            let name = document.getElementById('name').value.trim();
-            let email = document.getElementById('email').value.trim();
-            let report = document.getElementById('report').value.trim();
-
-           
-            if (!name || !email || !report) {
-                alert("Please fill out all fields.");
-                return;
-            }
-
-           
-            let data = {
-                name: name,
-                email: email,
-                report: report
-            };
-
-          
-            let jsonData = JSON.stringify(data);
-
-      
-            let xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "../../../Controller/eport/Check.php", true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-          
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    if (xhttp.responseText === "successfull") {
-                        location.reload();
-                    } else {
-                        location.reload();
-                    }
-                }
-            };
-
-            // Send data to the server
-            xhttp.send("final=" + encodeURIComponent(jsonData));
-        });
+    <script src="../../../assets/UserProfile/Help/contact.js">
     </script>
 
 </body>
